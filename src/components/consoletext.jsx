ef454dc23d7e5b38a50628b0de0f4a01f30ec786
kaushal digraskar <kaushaldigraskar7799@gmail.com>
2024-03-10 00:45:27 +0530

    added imports

--- src/components/consoletext.jsx
+++ src/components/consoletext.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
-import './consolText.css'
+import React, { useEffect } from "react";
+import "./consolText.css";
+import profilePhoto from "../assets/images/profile6.png";
+
 const ConsoleText = ({ words, id, colors }) => {
   useEffect(() => {
     let visible = true;
@@ -7,7 +9,7 @@ const ConsoleText = ({ words, id, colors }) => {
     let x = 1;
     let waiting = false;
     const target = document.getElementById(id);
-    target.setAttribute('style', `color: ${colors[0]}`);
+    target.setAttribute("style", `color: ${colors[0]}`);
 
     const interval = setInterval(() => {
       if (letterCount === 0 && !waiting) {
@@ -19,7 +21,7 @@ const ConsoleText = ({ words, id, colors }) => {
           const usedWord = words.shift();
           words.push(usedWord);
           x = 1;
-          target.setAttribute('style', `color: ${colors[0]}`);
+          target.setAttribute("style", `color: ${colors[0]}`);
           letterCount += x;
           waiting = false;
         }, 1000);
@@ -39,29 +41,29 @@ const ConsoleText = ({ words, id, colors }) => {
     return () => clearInterval(interval);
   }, [words, id, colors]);
 
-  return <div className=' container-fluid text-white '>
-    <div className='row'>
-
-    <div className=" col mt-3 d-flex flex-column  items-center justify-around  gap-3  font-poppins">
-  <h2 style={{fontSize:'3.5em'}}>
-    I'm Kaushal
-</h2>
-<h2 id='text' className='word-play'>
-  </h2>
-  <p >
-  As an accomplished software developer, I bring to the table a wealth of experience in developing and maintaining
-software using a wide range of programming languages and tools. With a proven ability to write clean and efficient code,
-I am adept at collaborating with cross-functional teams. My expertise extends across various domains, including mobile
-apps, web apps, and machine learning, making me a valuable asset for any organization seeking top-tier talent in
-software development.
-
-  </p>
-  </div>
-  <div className="col">
-  <img className='image' alt="" src="src/assets/images/profile6.png"/>
-  </div>
+  return (
+    <div className=" container-fluid text-white ">
+      <div className="row">
+        <div className=" col mt-3 d-flex flex-column  items-center justify-around  gap-3  font-poppins">
+          <h2 style={{ fontSize: "3.5em" }}>I'm Kaushal</h2>
+          <h2 id="text" className="word-play"></h2>
+          <p>
+            As an accomplished software developer, I bring to the table a wealth
+            of experience in developing and maintaining software using a wide
+            range of programming languages and tools. With a proven ability to
+            write clean and efficient code, I am adept at collaborating with
+            cross-functional teams. My expertise extends across various domains,
+            including mobile apps, web apps, and machine learning, making me a
+            valuable asset for any organization seeking top-tier talent in
+            software development.
+          </p>
+        </div>
+        <div className="col">
+          <img className="image" alt="" src={profilePhoto} />
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default ConsoleText;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,42 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const navbar = () => {
   return (
     <header className="d-flex justify-content-around  pb-3 pt-3 bg-black  ">
-        <NavLink to="/" className="nameTag text-decoration-none">
-            <p className="blue-gradient_text">KD</p>
+      <NavLink to="/" className="nameTag text-decoration-none">
+        <p className="blue-gradient_text">KD</p>
+      </NavLink>
+      <nav className="d-flex gap-4  ">
+        <NavLink
+          to="/about"
+          style={{ textDecoration: "none" }}
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-white"
+          }
+        >
+          About
         </NavLink>
-        <nav className="d-flex gap-4  ">
-        <NavLink to="/about" style={{textDecoration:"none"}} className={({isActive})=>(isActive ? "text-blue-500" : "text-white")}>
-            About
+        <NavLink
+          to="/projects"
+          style={{ textDecoration: "none" }}
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-white"
+          }
+        >
+          Projects
         </NavLink>
-        <NavLink to="/projects" style={{textDecoration:"none"}} className={({isActive})=>(isActive ? "text-blue-500" : "text-white")}>
-            Projects
+        <NavLink
+          to="/contact"
+          style={{ textDecoration: "none" }}
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-white"
+          }
+        >
+          Contact
         </NavLink>
-        </nav>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
